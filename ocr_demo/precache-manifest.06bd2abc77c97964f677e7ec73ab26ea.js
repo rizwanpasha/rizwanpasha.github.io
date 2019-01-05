439450fd268cf5583c7f6f95fe7b8d3317325e4e
rizwan pasha <rizwanpasha793@gmail.com>
2019-01-05 20:44:06 +0530

    updated homepage URL for ocr_demo

---
+++ ocr_demo/precache-manifest.06bd2abc77c97964f677e7ec73ab26ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a65a525826c91751ef07",
+    "url": "/ocr_demo/static/js/runtime~main.a65a5258.js"
+  },
+  {
+    "revision": "5b0bf70ce79f9f0e5830",
+    "url": "/ocr_demo/static/js/main.5b0bf70c.chunk.js"
+  },
+  {
+    "revision": "2488a686593183c78196",
+    "url": "/ocr_demo/static/js/1.2488a686.chunk.js"
+  },
+  {
+    "revision": "5b0bf70ce79f9f0e5830",
+    "url": "/ocr_demo/static/css/main.cc014c8d.chunk.css"
+  },
+  {
+    "revision": "94435176c25b60cfc4ee938c9b0eeaef",
+    "url": "/ocr_demo/index.html"
+  }
+];
